768743a9aaa1a6e4c3cb811ceb177b90bdd730f7
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-12-19 16:51:30 -0500

    Entidad respuesta

---
+++ Application Layer/DTOs/Response/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Layer.DTOs.Response
+{
+    public class Response<T> where T : class
+    {
+        public bool Estado {  get; set; }
+        public string? Mensaje { get; set; }
+        public T? Entidad { get; set; }
+    }
+}
