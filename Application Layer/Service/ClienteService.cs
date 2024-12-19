04d9adb3f5a90ee165a9520e4eccd002e4e96a17
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-12-19 16:29:34 -0500

    Creacion cliente servicio

---
+++ Application Layer/Service/ClienteService.cs
@@ -0,0 +1,32 @@
+﻿using Domain_Layer.Entities;
+using Domain_Layer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Layer.Service
+{
+    public class ClienteService
+    {
+        private readonly IClienteRepository _cliente;
+        public ClienteService(IClienteRepository cliente)
+        {
+            _cliente = cliente;
+        }
+
+        public async Task<string> GetCliente(Cliente cliente) 
+        {
+            var emailExiste = await _cliente.GetFirstOrDefault(c => c.Email == cliente.Email);
+            if (emailExiste != null) 
+            {
+                throw new Exception("El email ya está registrado.");
+            }
+            await _cliente.Add(cliente);
+            await _cliente.SaveAsync();
+
+            return "";
+        }
+    }
+}
